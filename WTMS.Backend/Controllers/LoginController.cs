d67d8d8a06cdd65cd9d704b2f2e633f062373c0e
Real James Gu <james.gu@pwc.com>
2017-08-14 18:19:30 +0800

    adding config

--- WTMS.Backend/Controllers/LoginController.cs
+++ WTMS.Backend/Controllers/LoginController.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using WTMS.DataAccess.ViewModel;
 
 namespace WTMS.Backend.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel loginModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginModel);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Login");
         }
     }
 }
